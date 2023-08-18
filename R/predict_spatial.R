5db1924d8368b25d090386672567e9cc117d43a9
Jason Connelly <50049077+jpconnel@users.noreply.github.com>
2023-08-18 11:02:22 -0700

    Update predict_spatial.R

--- R/predict_spatial.R
+++ R/predict_spatial.R
@@ -65,9 +65,15 @@ predict_spatial = function(newdata, learner, chunksize = 200L, format = "terra",
 
     mlr3misc::pmap(list(bs$cells_seq, bs$cells_to_read, seq_along(bs$cells_seq)), function(cells_seq, cells_to_read, n) {
 
-      print("1")
       stack = task$backend$stack
-      print("2")
+      print(bs)
+      print(cells_seq)
+      print(cells_to_read)
+      print(n)
+      print(cells_seq:((cells_seq + cells_to_read - 1)))
+      print(dim(stack))
+      print(length(stack))
+      
       pred = learner$predict(task, row_ids = cells_seq:((cells_seq + cells_to_read - 1)))
       print(dim(pred$prob))
       print(dim(pred$prob[,learner$learner$state$train_task$positive]))
