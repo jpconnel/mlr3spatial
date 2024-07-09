7cad9c041b8d4c4703663e28f771d6ba9d5c371a
Jason Connelly <50049077+jpconnel@users.noreply.github.com>
2024-07-09 11:24:21 -0700

    Update predict_spatial.R

--- R/predict_spatial.R
+++ R/predict_spatial.R
@@ -35,7 +35,6 @@ predict_spatial = function(newdata, learner, chunksize = 200L, format = "terra",
   task = as_task_unsupervised(newdata)
   assert_multi_class(task$backend, c("DataBackendRaster", "DataBackendVector"))
   assert_learner(learner)
-  print(task$positive)
 
   if (test_class(task$backend, "DataBackendRaster")) {
     assert_number(chunksize)
